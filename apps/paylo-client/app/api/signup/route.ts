b6e6aab7a525f9f64154f8096052ab9bdfc9facf
GeekyProgrammer07 <sumansamantaiitkgp@gmail.com>
2025-12-17 04:07:01 +0530

    updated signup logic

--- apps/paylo-client/app/api/signup/route.ts
+++ apps/paylo-client/app/api/signup/route.ts
@@ -1,16 +1,20 @@
 import bcrypt from "bcrypt";
 import prisma from "@paylo/db/client";
+import * as z from "zod";
+import { SignupSchema } from "@paylo/types";
+import Randomstring from "randomstring";
 
 export async function POST(req: Request) {
-  // TODO: Add Zod
-  // TODO: After Zod have error response like { error: "User exists" }
-  // TODO: Give user some money
-  const { name, phone, email, password } = await req.json();
+  const parsedInputs = SignupSchema.safeParse(await req.json());
+  if (!parsedInputs.success) {
+    const errors = z.treeifyError(parsedInputs.error);
+    return Response.json({ errors }, { status: 400 });
+  }
+
+  const { name, phone, email, password } = parsedInputs.data;
 
   const existingUser = await prisma.user.findFirst({
-    where: {
-      number: phone,
-    },
+    where: { number: phone },
   });
 
   if (existingUser) {
@@ -19,16 +23,39 @@ export async function POST(req: Request) {
 
   const hashed = await bcrypt.hash(password, 10);
 
-  await prisma.user.create({
-    data: {
-      name,
-      number: phone,
-      email,
-      password: hashed,
-    },
+  const amount =
+    (Math.floor(Math.random() * (100000 - 10000 + 1)) + 10000) * 100;
+
+  await prisma.$transaction(async (tx) => {
+    const user = await tx.user.create({
+      data: {
+        name,
+        number: phone,
+        email,
+        password: hashed,
+      },
+    });
+
+    await tx.onRampTransaction.create({
+      data: {
+        uuid: String(user.uuid),
+        status: "Success",
+        token: Randomstring.generate({ length: 10 }),
+        provider: "Axis Bank",
+        amount,
+        startTime: new Date(),
+        userId: user.id,
+      },
+    });
+
+    await tx.balance.create({
+      data: {
+        userId: user.id,
+        balance: amount,
+        locked: 0,
+      },
+    });
   });
 
-  return Response.json({
-    success: true,
-  });
+  return Response.json({ success: true });
 }
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,2 +1,3 @@
 export * from './bank-webhook';
 export * from './auth';
+export * from './transaction'
